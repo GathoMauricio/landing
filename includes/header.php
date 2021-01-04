498bd014ca096252f999f74cc085764218599808
Oscar Mauricio Juarez Soriano <mauricio2769@gmail.com>
2021-01-03 21:47:38 -0600

    inicio

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="ES">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <title>Landing page</title>
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font.css">
+    <link href="css/main.css" rel="stylesheet">
+</head>
+<body>
